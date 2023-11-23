65c0a0b10ddc36bc2e3c221c343fdb486864a162
PogebD <dima.p03.13.03@gmail.com>
2023-11-23 21:18:23 +0700

    Добавьте файлы проекта.

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <asp:Label ID="Label1" runat="server" Text="Задание №1"></asp:Label>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Получить информацию о деталях, которых в настоящий момент
+не хватает для изготовления заданного количества указанного изделия."></asp:Label>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Введите название изделия:"></asp:Label>
+            <br />
+            <asp:DropDownList ID="JobDropDownList1" runat="server" OnSelectedIndexChanged="JobDropDownList1_SelectedIndexChanged"> 
+
+            </asp:DropDownList>
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Введите Количество изделий:"></asp:Label>
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server" ></asp:TextBox>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private string _jobName
+        {
+            get => ViewState["_jobName"] as string ?? string.Empty;
+            set => ViewState["_jobName"] = value;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Создаем объект подключения
+            OdbcConnection conn = new OdbcConnection();
+            // Задаем параметр подключения – имя ODBC-источника
+            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+        
+            // Подключаемся к БД
+            conn.Open();
+            // Определяем строку с текстом запроса
+            string strSQL = "SELECT name FROM pmib0413.j";
+            // Создаем объект запроса
+            OdbcCommand cmd = new OdbcCommand(strSQL, conn);
+
+            OdbcTransaction tx = null;
+            try
+            {
+                // Начинаем транзакцию и извлекаем объект транзакции из объекта подключения.
+                tx = conn.BeginTransaction();
+                // Включаем объект SQL-команды в транзакцию
+                cmd.Transaction = tx;
+                // Выполняем SQL-команду и получаем количество обработанных записей
+                OdbcDataReader i = cmd.ExecuteReader();
+                // Подтверждаем транзакцию  
+                tx.Commit();
+                while(i.Read())
+                {
+                    var jobName = i["name"].ToString();
+
+                    JobDropDownList1.Items.Add(new ListItem(jobName));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                // При возникновении любой ошибки 
+                // Формируем сообщение об ошибке 
+                Label1.Text = ex.Message;
+                // выполняем откат транзакции 
+                tx.Rollback();
+            }
+
+            //закрываем соединение
+            conn.Close();
+
+            
+        }
+
+        protected void JobDropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(JobDropDownList1.SelectedIndex >= 0)
+            {
+                _jobName = JobDropDownList1.SelectedValue;
+                Label4.Text = _jobName;
+            }
+        }
+    }
+}
